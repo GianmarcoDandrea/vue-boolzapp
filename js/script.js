471fafc344287853f022eb72d3ad32e46bf61963
GianmarcoDandrea <gianm96@hotmail.it>
2023-11-17 11:29:47 +0100

    update: milestone 2

--- js/script.js
+++ js/script.js
@@ -6,13 +6,20 @@ const { createApp } = Vue
       return {
 
         contacts: listContacts,
-        
         activeIndex : 0,
 
       }
     },
 
     methods: {
+      changeActiveIndex(index) {
+        this.activeIndex = index;
+      },
+
+      changeDate (fullDate) {
+        const luxonDate = dt.fromFormat(fullDate, "dd/MM/yyyy HH:mm:ss");
+        return luxonDate.toFormat("HH:mm");
+      },
 
     }
 
